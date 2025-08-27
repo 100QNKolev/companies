084f9d1bc3df6c930e415304f2957fce158fb8a8
100QN <83006473+100QNKolev@users.noreply.github.com>
2025-08-27 18:13:47 +0300

    Added database packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,8 @@ dependencies {
     implementation("io.quarkus:quarkus-resteasy-jackson")
     implementation("io.quarkus:quarkus-arc")
     implementation("io.quarkus:quarkus-resteasy")
+    implementation("io.quarkus:quarkus-hibernate-orm")
+    implementation("io.quarkus:quarkus-jdbc-postgresql")
     testImplementation("io.quarkus:quarkus-junit5")
     testImplementation("io.rest-assured:rest-assured")
 }

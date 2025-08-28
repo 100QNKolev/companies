63db191905da0a2ac8e14f765bb942af06806c61
100QN <83006473+100QNKolev@users.noreply.github.com>
2025-08-28 13:23:05 +0300

    Added PATH function to the company service

--- src/main/java/org/companiesOnMarket/service/CompanyService.java
+++ src/main/java/org/companiesOnMarket/service/CompanyService.java
@@ -2,8 +2,11 @@ package org.companiesOnMarket.service;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
 import org.companiesOnMarket.entity.Company;
+import org.companiesOnMarket.error.NotFoundException;
 import org.companiesOnMarket.repository.CompanyRepository;
+import org.companiesOnMarket.util.CompanyValidator;
 
 import java.time.Instant;
 import java.util.List;
@@ -16,10 +19,45 @@ public class CompanyService {
 
     public List<Company> getAllCompanies() { return repo.getAll(); }
 
+    @Transactional
     public void createCompany(Company company)
     {
         // TODO: Add authorization
         company.setCreatedAt(Instant.now());
+
+        CompanyValidator.validateInput(company);
+
         repo.create(company);
+    }
+
+    @Transactional
+    public Company updateCompany(long id, Company companyToUpdate)
+    {
+        Company existingCompany = repo.findById(id);
+
+        if (existingCompany == null)
+        {
+            throw new NotFoundException("Company not found");
+        }
+
+        //CompanyValidator.validateInput(companyToUpdate);
+
+        if (companyToUpdate.getName() != null && !companyToUpdate.getName().isBlank()) {
+            existingCompany.setName(companyToUpdate.getName());
+        }
+        if (companyToUpdate.getCountry() != null && !companyToUpdate.getCountry().isBlank()) {
+            existingCompany.setCountry(companyToUpdate.getCountry());
+        }
+        if (companyToUpdate.getSymbol() != null && !companyToUpdate.getSymbol().isBlank()) {
+            existingCompany.setSymbol(companyToUpdate.getSymbol());
+        }
+        if (companyToUpdate.getEmail() != null) {
+            existingCompany.setEmail(companyToUpdate.getEmail());
+        }
+        if (companyToUpdate.getWebsite() != null) {
+            existingCompany.setWebsite(companyToUpdate.getWebsite());
+        }
+
+        return existingCompany;
     }
 }

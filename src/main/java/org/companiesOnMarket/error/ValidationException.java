5d2b3866fbc881ece8457d27a5e5deee57b89528
100QN <83006473+100QNKolev@users.noreply.github.com>
2025-08-28 11:52:21 +0300

    Created new validation exception

---
+++ src/main/java/org/companiesOnMarket/error/ValidationException.java
@@ -0,0 +1,8 @@
+package org.companiesOnMarket.error;
+
+public class ValidationException extends RuntimeException {
+    public ValidationException(String message) {
+        super(message);
+    }
+}
+

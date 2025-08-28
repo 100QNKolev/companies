e9912e8833f1ae9693c2a67975fe17b3a29e56c8
100QN <83006473+100QNKolev@users.noreply.github.com>
2025-08-28 13:22:33 +0300

    Added find by id in company repository

--- src/main/java/org/companiesOnMarket/repository/CompanyRepository.java
+++ src/main/java/org/companiesOnMarket/repository/CompanyRepository.java
@@ -4,7 +4,6 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
-import jakarta.transaction.Transactional;
 import org.companiesOnMarket.entity.Company;
 import java.util.List;
 
@@ -14,16 +13,14 @@ public class CompanyRepository {
     @Inject
     EntityManager em;
 
-    @Transactional
-    public void create(Company company)
-    {
-        em.persist(company);
-    }
+    public void create(Company company) { em.persist(company); }
 
     public List<Company> getAll()
     {
         TypedQuery<Company> query = em.createQuery("from Company", Company.class);
         return query.getResultList();
     }
+
+    public Company findById(long id) { return em.find(Company.class, id); }
 
 }

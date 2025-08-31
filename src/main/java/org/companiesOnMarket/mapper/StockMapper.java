f0f556dfba51b0fde4dc56a32c42418b8e2faa16
100QN <83006473+100QNKolev@users.noreply.github.com>
2025-08-31 14:39:56 +0300

    Created mapper

---
+++ src/main/java/org/companiesOnMarket/mapper/StockMapper.java
@@ -0,0 +1,19 @@
+package org.companiesOnMarket.mapper;
+
+import org.companiesOnMarket.dto.CompanyGetStocksDto;
+import org.companiesOnMarket.dto.StockGetDto;
+import org.companiesOnMarket.entity.Company;
+import org.companiesOnMarket.entity.Stock;
+import org.mapstruct.*;
+
+@Mapper(componentModel = "cdi")
+public interface StockMapper {
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateEntityFromDto(StockGetDto dto, @MappingTarget Stock entity);
+
+    @Mapping(source = "companyStock.marketCapitalization", target = "marketCapitalization")
+    @Mapping(source = "companyStock.shareOutstanding", target = "shareOutstanding")
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void createCompanyStockResult(Company entity, @MappingTarget CompanyGetStocksDto dto);
+}

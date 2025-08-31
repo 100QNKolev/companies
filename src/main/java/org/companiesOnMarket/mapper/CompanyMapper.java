6075c3e97c381c9564ed20d13eef1ccb54efccdb
100QN <83006473+100QNKolev@users.noreply.github.com>
2025-08-31 15:06:16 +0300

    Added methods to suit the new logic

--- src/main/java/org/companiesOnMarket/mapper/CompanyMapper.java
+++ src/main/java/org/companiesOnMarket/mapper/CompanyMapper.java
@@ -1,12 +1,14 @@
 package org.companiesOnMarket.mapper;
 
 import org.companiesOnMarket.dto.CompanyCreateDto;
+import org.companiesOnMarket.dto.CompanyGetDto;
 import org.companiesOnMarket.dto.CompanyUpdateDto;
 import org.companiesOnMarket.entity.Company;
 import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
+import java.util.List;
 
 @Mapper(componentModel = "cdi")
 public interface CompanyMapper {
@@ -15,4 +17,8 @@ public interface CompanyMapper {
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     void updateEntityFromDto(CompanyUpdateDto dto, @MappingTarget Company entity);
+
+    void createGetCompanyResultDto(Company entity, @MappingTarget CompanyGetDto dto);
+
+    List<CompanyGetDto> toGetCompanyDtoList(List<Company> companies);
 }
